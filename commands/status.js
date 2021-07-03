27a9e2acf87c5b773a6d57a203271b72fc11b857
Henry Zheng <73442345+nullZheng@users.noreply.github.com>
2021-07-03 14:50:19 -0400

    Add files via upload

---
+++ commands/status.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "status",
+    description: "returns bot information and credits",
+    async execute(message, args){
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle("`Bot Information:`")
+        embed.setThumbnail("https://i.imgur.com/Y6rAoZi.png");
+        embed.addField("Version:", "`Alpha`");
+        embed.addField("Status:", "`Online`");
+        embed.addField("Developer:", "<@538523256668291092>");
+        embed.setColor("#F1C40F");
+        await message.channel.send(embed)
+    }
+}
